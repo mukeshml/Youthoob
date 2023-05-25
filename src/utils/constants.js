8f1ee35dcc162a2f1dc0525e13d56efedb0c9d85
Mukesh Kumar Mandal <103560858+mukeshml@users.noreply.github.com>
2023-05-25 17:24:32 +0530

    Add files via upload

---
+++ src/utils/constants.js
@@ -0,0 +1,39 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SchoolIcon from '@mui/icons-material/School';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+
+export const logo = 'https://i2.wp.com/img.aapks.com/imgs/e/f/8/ef8d1e99db4aff7b6946c587be88ced5_icon.png';
+
+export const categories = [
+  { name: 'New', icon: <HomeIcon />, },
+
+  { name: 'Music', icon: <MusicNoteIcon /> },
+  { name: 'Education', icon: <SchoolIcon />, },
+  { name: 'Podcast', icon: <GraphicEqIcon />, },
+  { name: 'Movie', icon: <OndemandVideoIcon />, },
+  { name: 'Gaming', icon: <SportsEsportsIcon />, },
+  { name: 'Live', icon: <LiveTvIcon />, },
+  { name: 'Sport', icon: <FitnessCenterIcon />, },
+  { name: 'Fashion', icon: <CheckroomIcon />, },
+  { name: 'Beauty', icon: <FaceRetouchingNaturalIcon />, },
+  { name: 'Comedy', icon: <TheaterComedyIcon />, },
+  { name: 'Gym', icon: <FitnessCenterIcon />, },
+  { name: 'Crypto', icon: <DeveloperModeIcon />, },
+];
+
+export const demoThumbnailUrl = 'https://i.ytimg.com/vi/tn7WjlJFXQA/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAgoMQ9c0ukEdttDNow7I8WfmM8Cg';
+export const demoChannelUrl = '/channel/UCbdK0SuTgZa798i7OSx2TFQ';
+export const demoVideoUrl = '/video/tn7WjlJFXQA';
+export const demoChannelTitle = 'Glow-up Sis';
+export const demoVideoTitle = 'My Ist Year medical experience[BHMS] +Final exam Reaction 🥶| Myths & lies abt medical life 🧠🫀';
+export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
